f96ed5507c8580854ba3d1ca2f3e5705555fa058
ykonno <konchan1129@gmail.com>
2018-07-13 16:22:20 +0900

    add fetch data from boot endpoint.

---
+++ pages/member/_id.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container">
+    <p>{{member[1].id}}</p>
+    <p>{{member[1].name}}</p>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    async asyncData({
+      params
+    }) {
+      const {
+        data: member
+      } = await axios.get('http://localhost:8080/user', {
+    //   } = await axios.get(`https://api.github.com/users/${params.id}`, {
+          proxy: false
+      })
+      return {
+        member
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+  }
+
+  .container img {
+    margin-bottom: 20px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+</style>
